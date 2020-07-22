271848bd903420d34b5cc76adad1c24ac6d39442
zhanwang-sky <zhanwang-sky@users.noreply.github.com>
2020-07-22 20:08:36 +0800

    Initial Commit

---
+++ quic-apps/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  quic-apps
+//
+//  Created by 冀宸 on 2020/7/22.
+//  Copyright © 2020 冀宸. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
